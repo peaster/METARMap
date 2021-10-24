fe4641b14d10f55d780b9b9a35cadf187d50b8bd
Paul Easterbrooks <peaster@pauls-mbp-2.myfiosgateway.com>
2021-10-24 10:33:02 -0400

    implement metarmap using python classes

--- metar-service.py
+++ metar-service.py
@@ -1,10 +1,13 @@
 import paho.mqtt.client as mqtt
 import mqtt_config as config
 import os
+from metar import MetarMap
 
 broker_url = config.broker_url
 broker_port = config.broker_port
 state = "OFF"
+
+map = MetarMap(0.17)
 
 def on_connect(client, userdata, flags, rc):
    print("Connected With Result Code: {}".format(rc))
@@ -13,10 +16,10 @@ def on_message(client, userdata, message):
     decodedMessage = message.payload.decode()
     print("Message received from broker: "+ decodedMessage)
     if decodedMessage == 'ON':
-        os.system('sudo python3 metar.py')
+        map.updateLights()
         state = "ON"
     elif (decodedMessage == 'OFF') :
-        os.system('sudo ./lightsoff.sh')
+        map.shutdownLights()
         state = "OFF"
 
 client = mqtt.Client('metarmap')
